1be667cefc703ef752c59e14dffd858c8dd0bb5e
STRANGE_20.03 <josephangaspilco478@gmail.com>
2026-06-18 00:34:52 -0500

    ACTUALIZACION

---
+++ MoansoProyecto/CapaEntidad/CapaEntidad/entVehiculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class entVehiculo
+    {
+        public int IdVehiculo { get; set; }
+        public int IdCliente { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+        public string Placa { get; set; }
+        public string Color { get; set; }
+        public bool Activo { get; set; }
+        public System.DateTime FechaRegistro { get; set; }
+
+        // Para mostrar nombre del cliente
+        public string NombreCliente { get; set; }
+    }
+}
